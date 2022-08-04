b284bace07e3ae14c5596a0732959606ecb7676e
BWilliams07 <104173554+BWilliams07@users.noreply.github.com>
2022-08-03 23:13:20 -0400

    Changes

--- src/main/java/JobData.java
+++ src/main/java/JobData.java
@@ -18,6 +18,7 @@ public class JobData {
 
     public static ArrayList<String> findAll(String field) {
 
+        // load data, if not already loaded
         loadData();
 
         ArrayList<String> values = new ArrayList<>();
@@ -35,13 +36,16 @@ public class JobData {
 
     public static ArrayList<HashMap<String, String>> findAll() {
 
+        // load data, if not already loaded
         loadData();
 
         return allJobs;
     }
 
+
     public static ArrayList<HashMap<String, String>> findByColumnAndValue(String column, String value) {
 
+        // load data, if not already loaded
         loadData();
 
         ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
@@ -58,3 +62,45 @@ public class JobData {
         return jobs;
     }
 
+    /**
+     * Read in data from a CSV file and store it in a list
+     */
+    private static void loadData() {
+
+        // Only load data once
+        if (isDataLoaded) {
+            return;
+        }
+
+        try {
+
+            // Open the CSV file and set up pull out column header info and records
+            Reader in = new FileReader(DATA_FILE);
+            CSVParser parser = CSVFormat.RFC4180.withFirstRecordAsHeader().parse(in);
+            List<CSVRecord> records = parser.getRecords();
+            int numberOfColumns = records.get(0).size();
+            String[] headers = parser.getHeaderMap().keySet().toArray(new String[numberOfColumns]);
+
+            allJobs = new ArrayList<>();
+
+            // Put the records into a more friendly format
+            for (CSVRecord record : records) {
+                HashMap<String, String> newJob = new HashMap<>();
+
+                for (String headerLabel : headers) {
+                    newJob.put(headerLabel, record.get(headerLabel));
+                }
+
+                allJobs.add(newJob);
+            }
+
+            // flag the data as loaded, so we don't do it twice
+            isDataLoaded = true;
+
+        } catch (IOException e) {
+            System.out.println("Failed to load job data");
+            e.printStackTrace();
+        }
+    }
+
+}
